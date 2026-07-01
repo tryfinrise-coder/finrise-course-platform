1d1d97df9f11aef38c76b4df239c34dc31d21e89
Jaydeepsaha2003 <154578215+Jaydeepsaha2003@users.noreply.github.com>
2026-07-01 12:03:18 +0530

    Track storage folder in git and add Microsoft Clarity tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,15 @@ export default async function RootLayout({
       </head>
       <body>
         {children}
+        {/* Microsoft Clarity */}
+        <Script id="ms-clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window,document,"clarity","script","xfieo1ijol");`}
+        </Script>
+
         {/* Meta Pixel */}
         <Script id="meta-pixel" strategy="afterInteractive">
           {`!function(f,b,e,v,n,t,s)
